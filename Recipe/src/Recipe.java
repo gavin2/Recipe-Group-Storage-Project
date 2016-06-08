8e80869f3159afead1df64968eb7ed157dbf87cf
085685858 <085685858@109R030573XBQ22.EDU.YRDSB.NET>
2016-06-08 14:36:16 -0400

    Merge origin/master
    Conflicts:
    	src/calculatzarsep2015/StringCalculator.java
    	src/unitConversionCalculator/unitConversion.java

--- Recipe/src/Recipe.java
+++ Recipe/src/Recipe.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,5 +11,6 @@
  * @author 085685858
  */
 public class Recipe {
-    
+    ArrayList ingredients;
+    ArrayList steps;
 }
