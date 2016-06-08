b111248feb24e099fb0c16a7ca117661a6b8bbdd
085685858 <085685858@109R030573XBQ22.EDU.YRDSB.NET>
2016-06-08 14:23:51 -0400

    Merge origin/master
    Conflicts:
    	src/calculatzarsep2015/StringCalculator.java
    	src/unitConversionCalculator/unitConversion.java

---
+++ Recipe/src/Main.java
@@ -0,0 +1,30 @@
+
+/**
+ *
+ * @author Gavin Christie
+ */
+public class Main {
+
+    /**
+     * File IO
+     *  - Adding recipes, reading recipes, editing recipes
+     * 
+     * Searching for recipes
+     * 
+     * Generate shopping lists
+     *  - Recipe scaling
+     * 
+     * 
+     * 
+     * GUI - Last Please
+     * 
+     */
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
